be354bc717cb44594c9a71131ca4216615632adf
yellowflash2041 <yellowflash2041@gmail.com>
2022-11-18 01:07:48 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById("mobilemenu");
+const mobilenav = document.getElementById("mobilenav");
+
+mobilenav.style.left = "-100%";
+
+btn.onclick = function () {
+  if (mobilenav.style.left == "-100%") {
+    mobilenav.style.left = "50%";
+    btn.src = "images/icon-close.svg";
+  } else {
+    mobilenav.style.left = "-100%";
+    btn.src = "images/icon-hamburger.svg";
+  }
+};
